5718eff48de4f6ed5b273fb2cc1d58a0e5196e62
sebG <seb.goodchild@gmail.com>
2025-10-30 00:00:46 -0400

    Changed the caret size element_height and the space between carets element_offset, change age_image layout to accommodate

---
+++ js/majorGodSelection.js
@@ -0,0 +1,18 @@
+// let majorGodSelectionPanel = document.getElementById('major_god_selection_panel__fixed');
+
+// let rootEl = document.getElementById('root');
+// console.log('rootEl: ', rootEl);
+// console.log('majorGodSelectionPanel: ', majorGodSelectionPanel);
+// console.log('majorGodSelectionPanel.offsetHeight: ', majorGodSelectionPanel.offsetHeight);
+// console.log('majorGodSelectionPanel.clientHeight: ', majorGodSelectionPanel.clientHeight);
+// console.log('rootEl.clientHeight: ', rootEl.clientHeight);
+
+
+// console.log('majorGodSelectionPanel.clientHeight: ', majorGodSelectionPanel.clientHeight);
+// console.log('majorGodSelectionPanel.offsetHeight: ', majorGodSelectionPanel.offsetHeight);
+// majorGodSelectionPanel.style.height = rootEl.clientHeight + 'px';
+// majorGodSelectionPanel.style.width = '300px';
+// console.log(majorGodSelectionPanel.offsetWidth);
+// console.log('majorGodSelectionPanel.clientHeight: ', majorGodSelectionPanel.clientHeight);
+// console.log('majorGodSelectionPanel.offsetHeight: ', majorGodSelectionPanel.offsetHeight);
+// console.log('typeof(rootEl.clientHeight): ',typeof(rootEl.clientHeight));
