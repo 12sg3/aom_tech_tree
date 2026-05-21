3021cb4cd37d250845b48445da261738a2dd277c
sebG <seb.goodchild@gmail.com>
2026-05-20 23:54:08 -0400

    updates to connections and major god lane matrices for chinese greeks egyptians

--- src/ts/techtree.ts
+++ src/ts/techtree.ts
@@ -325,7 +325,7 @@ export class Lane {
             if (carets.has(from) && carets.get(from).x > Math.max(...(allRelevantTos.map(to_ => carets.get(to_).x)))) {
                 console.assert(allRelevantTos.length === 1, `Overlapping carets: ${allRelevantTos}`)
                 if (allRelevantTos.length === 1) {
-                    console.log('allRelevantTos lenght === 1: ', allRelevantTos);
+                    console.log('allRelevantTos length === 1: ', allRelevantTos);
                 } else {
                     console.log('allRelevantTos .length !== 1: ', allRelevantTos);
                 }
@@ -903,7 +903,7 @@ export function getConnections() {
     } else {
         connectionsToAdd = [];
     }
-
+    console.log('connectionsToAdd: ', connectionsToAdd, SELECTED_MAJOR_GOD_ID);
     // connectionsToAdd = connectionsToAddMatrices.odin; // change to be dynamic
 
     for (let i = 0; i < connectionsToAdd.length; i++) {
