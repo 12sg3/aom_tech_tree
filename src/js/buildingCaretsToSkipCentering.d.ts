ee7b3be5d4d1fd08bd379e1fa460e8feb05d0ba4
sebG <seb.goodchild@gmail.com>
2026-05-22 01:44:15 -0400

    add a file that contains the buildsCarets to not center. Added a logic check to this file in lane.updatePositions

---
+++ src/js/buildingCaretsToSkipCentering.d.ts
@@ -0,0 +1,2 @@
+export declare const buildingCaretsToSkipCenteringForId: string[];
+//# sourceMappingURL=buildingCaretsToSkipCentering.d.ts.map
---
+++ src/js/buildingCaretsToSkipCentering.js
@@ -0,0 +1,4 @@
+import { BARRACKS, MONUMENT_TO_GODS, MONUMENT_TO_PHARAOHS, MONUMENT_TO_PRIESTS, MONUMENT_TO_SOLDIERS, MONUMENT_TO_VILLAGERS, TITAN_GATE } from "./units.js";
+const buildingCaretsToSkipCenteringFor = [TITAN_GATE, BARRACKS, MONUMENT_TO_VILLAGERS, MONUMENT_TO_SOLDIERS, MONUMENT_TO_PRIESTS, MONUMENT_TO_PHARAOHS, MONUMENT_TO_GODS];
+export const buildingCaretsToSkipCenteringForId = buildingCaretsToSkipCenteringFor.map(caret => `building_${String(caret.id)}`);
+//# sourceMappingURL=buildingCaretsToSkipCentering.js.map
