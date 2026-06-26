ec8500444f691a1ae169a2fe81794edd0e7637e3
sebG <seb.goodchild@gmail.com>
2026-06-26 16:04:28 -0400

    added string_table data handling to get proto-xml script. Refactored directory structure for game_data.

---
+++ game_data/scripts/get-proto-xml-data.py
@@ -0,0 +1,241 @@
+import xml.etree.ElementTree as ET
+import json 
+from pathlib import Path
+
+UNIT = 'unit'
+BUILDING = 'building'
+UNIT_CLASS = 'UnitClass'
+BUILDING_CLASS = 'BuildingClass'
+
+NAME = 'Name'
+TYPE = 'Type'
+SUB_TYPE = 'Sub_Type'
+FACTION = 'Faction'
+FOOD_COST = 'Food_Cost'
+WOOD_COST = 'Wood_Cost'
+GOLD_COST = 'Gold_Cost'
+FAVOR_COST = 'Favor_Cost'
+POP_COST = 'Pop_Cost'
+TRAINING_TIME = 'Training_Time'
+BUILD_POINTS = 'Buildpoints'
+HITPOINTS = 'Hitpoints'
+LINE_OF_SIGHT = 'Line_of_Sight'
+ATTACK_TYPE = 'Attack_Type'
+RATE_OF_FIRE = 'Rate_of_fire'
+BONUS_MULTIPIER = 'Bonus_Multiplier'
+MAXIMUM_RANGE = 'Maximum_range' # maybe change to Maximum_Range (would need to change other files first)
+HACK_DAMAGE = 'Hack_Damage'
+PIERCE_DAMAGE = 'Pierce_Damage'
+DIVINE_DAMAGE = 'Divine_Damage'
+CRUSH_DAMAGE = 'Crush_Damage'   
+HACK_ARMOR = 'Hack_Armor'
+PIERCE_ARMOR = 'Pierce_Armor'
+CRUSH_ARMOR = 'Crush_Armor'
+VELOCITY = 'Velocity'
+
+DISPLAY_NAME = 'Display_Name'
+MOVEMENT_TYPE = 'Movement_Type'
+WEIGHT_CLASS = 'Weight_Class'
+
+
+COST_TYPE_MAP = {
+    'Food' : FOOD_COST,
+    'Gold' : GOLD_COST,
+    'Wood' : WOOD_COST,
+    'Favor' : FAVOR_COST
+}
+
+ARMOR_TYPE_MAP = {
+    'Hack': HACK_ARMOR,
+    'Pierce': PIERCE_ARMOR,
+    'Crush': CRUSH_ARMOR
+}
+
+def get_protoaction_data(unit_tag):
+    p_actions = []        
+    for p_action in unit_tag.findall('protoaction'):
+        p_action_single_dict = {}
+        for item in p_action.iter('*'):
+            p_item_dict = {}
+            print('item: ', item)
+            print('item.tag: ', item.tag)
+            # p_item_dict['tag'] = item.tag
+            if item.text:
+                print('item.text.strip(): ', item.text.strip())
+                p_item_dict['text_value'] = item.text.strip()
+            print('item.attrib :', item.attrib)
+            if any(item.attrib.values()):
+                print('any - item.attrib: ', item.attrib)
+                p_item_dict['attributes'] = item.attrib
+            print('-------')
+            p_action_single_dict[item.tag] = p_item_dict
+        p_actions.append(p_action_single_dict)
+    return p_actions
+
+# "Name": "eagle warrior",
+#         "Type": "unit",
+#         "Sub_Type": null,
+#         "Faction": null,
+#         "Prefixes": null,
+#         "Suffixes": null,
+#         "Food_Cost": null,
+#         "Wood_Cost": 80.0,
+#         "Gold_Cost": 45.0,
+#         "Favor_Cost": null,
+#         "Pop_Cost": 3.0,
+#         "Training_Time": "21",
+#         "Buildpoints": null,
+#         "Hitpoints": 90.0,
+#         "Line_of_Sight": 20.0,
+#         "Attack_Type": "Ranged Attack",
+#         "Rate_of_fire": 1.5,
+#         "Bonus_Multiplier": " AbstractArcher:  1.50",
+#         "Maximum_range": 18.0,
+#         "Hack_Damage": null,
+#         "Pierce_Damage": 10.0,
+#         "Divine_Damage": null,
+#         "Crush_Damage": null,
+#         "Hack_Armor": 15.0,
+#         "Pierce_Armor": 30.0,
+#         "Crush_Armor": 99.0,
+#         "Velocity": "4.50 EEE",
+#         "Focus": null,
+#         "Description": "Elite Ranged soldier. Good against anything it can reach, especially ranged soldiers.",
+#         "God_Of": null,
+#         "Version_Suffix": null,
+#         "Parent_Caret_Name": null,
+#         "id": 74
+#     },
+
+
+# tree = ET.parse('game_data/proto.xml')
+tree = ET.parse('../game_files/proto.xml')
+
+
+print(tree)
+
+units_dict = {}
+buildings_dict = {}
+root = tree.getroot()
+print(root)
+print('root.findall("unit"):', root.findall("unit"))
+
+for unit_tag in root.findall(UNIT):
+    print('unit_tag: ', unit_tag)
+    new_dict_entry = {TYPE: None, NAME: ''}
+
+    # not using name atrb from unit tag as displaynameid is closer to the existing name schema
+    # also need to check if name is already added as their are unit tags with the same name, ex: Hoplite and HopliteSPC
+    # name_tag = unit_tag.get('name')
+    # new_dict_entry['name_tag'] = name_tag
+
+    if unit_tag.find('displaynameid') is not None:
+        name = unit_tag.find('displaynameid').text.replace('STR_UNIT_', '').replace('_NAME', '').replace('STR_BLD_', '').replace('STR_EMB_', '')
+        if name in units_dict.keys() or name in buildings_dict.keys():
+            continue
+        new_dict_entry[NAME] = unit_tag.find('displaynameid').text.replace('STR_UNIT_', '').replace('_NAME', '').replace('STR_BLD_', '').replace('STR_EMB_', '')
+        new_dict_entry[DISPLAY_NAME] = new_dict_entry[NAME].replace('_', ' ').lower()
+
+    unit_type_tags = unit_tag.findall('unittype')
+    print('unit_type_tags: ', unit_type_tags)
+    for tag in unit_type_tags:
+        tag_text = tag.text.strip()
+        print('tag.text: ', tag.text)
+        print('tag_text: ', tag_text)
+        if tag_text == UNIT_CLASS:
+            new_dict_entry[TYPE] = UNIT
+            print('unit found')
+            continue
+        if tag_text == BUILDING_CLASS:
+            new_dict_entry[TYPE] = BUILDING
+            print('building found')
+            continue
+    
+    if new_dict_entry[TYPE] == None:
+        print('None found')
+        continue
+
+     # Unit & Building
+    if unit_tag.find('movementtype') is not None:
+        new_dict_entry[MOVEMENT_TYPE] = unit_tag.find('movementtype').text
+
+    # if unit_tag
+
+    if unit_tag.find('initialhitpoints') is not None:
+        new_dict_entry[HITPOINTS] = unit_tag.find('initialhitpoints').text 
+
+    if unit_tag.find('trainpoints') is not None:
+        new_dict_entry[TRAINING_TIME] = unit_tag.find('trainpoints').text
+
+    if unit_tag.find('buildpoints') is not None:
+        new_dict_entry[BUILD_POINTS] = unit_tag.find('buildpoints').text
+
+    if unit_tag.find('weightclass') is not None:
+        new_dict_entry[WEIGHT_CLASS] = unit_tag.find('weightclass').text
+
+    if unit_tag.find('populationcount') is not None:
+        new_dict_entry[POP_COST] = unit_tag.find('populationcount').text
+
+    if unit_tag.find('maxvelocity') is not None:
+        new_dict_entry[VELOCITY] = unit_tag.find('maxvelocity').text
+
+    if unit_tag.find('los') is not None:
+        new_dict_entry[LINE_OF_SIGHT] = unit_tag.find('los').text
+
+    if unit_tag.find('cost') is not None:
+        for cost in unit_tag.findall('cost'):
+            cost_type = cost.get('resourcetype')
+            cost_amount = cost.text
+            new_dict_entry[COST_TYPE_MAP[cost_type]] = cost_amount
+
+    if unit_tag.find('armor') is not None:
+        for armor in unit_tag.findall('armor'):
+            armor_type = armor.get('type')
+            armor_amount = armor.get('value')
+            new_dict_entry[ARMOR_TYPE_MAP[armor_type]] = armor_amount
+        
+    if unit_tag.find('protoaction'): #is not None
+        new_dict_entry['protoactions'] = get_protoaction_data(unit_tag)
+
+    # LAST STEP
+    if new_dict_entry[TYPE] == UNIT:
+        units_dict[new_dict_entry[NAME]] = new_dict_entry
+    elif new_dict_entry[TYPE] == BUILDING:
+        buildings_dict[new_dict_entry[NAME]] = new_dict_entry
+
+with open('../extracted_data/ST_units_en_data.json') as file:
+    ST_unit_dict = json.load(file)
+
+# need to standardize name vs NAME
+for key in ST_unit_dict.keys():
+    if key in units_dict:
+        # units_dict[key]
+        for key_inner in ST_unit_dict[key]:
+            units_dict[key][key_inner] = ST_unit_dict[key][key_inner]
+
+with open('../extracted_data/ST_buildings_en_data.json') as file:
+    ST_building_dict = json.load(file)
+
+for key in ST_building_dict:
+    if key in buildings_dict:
+        for key_inner in ST_building_dict[key]:
+            buildings_dict[key][key_inner] = ST_building_dict[key][key_inner]
+
+
+print('units_dict: ', units_dict)
+print('buildings_dict: ', buildings_dict)
+print('-----------------------------')
+print(units_dict['SOUL_GUIDE'])
+print('------')
+print(units_dict['FAFNIR'])
+print('--------')
+
+units_dict_json = json.dumps(units_dict, indent=4)
+
+with open('../extracted_data/units-data-from-xml.json', 'w') as file:
+    file.write(units_dict_json)
+
+buildings_dict_json = json.dumps(buildings_dict, indent=4)
+
+with open('../extracted_data/buildings-data-from-xml.json', 'w') as file:
+    file.write(buildings_dict_json)
---
+++ game_data/scripts/string_table_handling.py
@@ -0,0 +1,223 @@
+import json
+from pathlib import Path
+
+# tech costs and effects --> in techtree.xlm
+# unit, building, tech descritpion --> str table
+# unit and building costs and stats --> proto.xml
+
+# godpowers --> find godpower file (I think it's one for each pathneon, I saw the Aztec one)
+
+units_dict = {}
+buildings_dict = {}
+techs_dict = {}
+
+id_index_counter = 0
+
+already_add_names = {}
+
+suffix_list = ['NAME', 'LR', 'SR']
+
+with open ('../game_files/string_table_en.txt') as str_tbl_file:
+
+    for line in str_tbl_file:
+        # if 'STR_UNIT' in line and ('NAME' in line or 'LR' in line or 'SR' in line):
+        if 'STR_UNIT' in line:
+            line_split_words = line.split(';')
+            line_split_words_str = []
+            use_Str_split = False
+            if ';' not in line:
+                line_split_words_str = line.split('Str')
+                use_Str_split = True
+                print('** inside line_split_words_str: ', line_split_words_str)
+            
+            # print('** outside line_split_words_str: ', line_split_words_str)
+            if use_Str_split == True:
+                print('*** inside line split str: ', line_split_words_str)
+                left_side = line_split_words_str[0].strip()
+                suffix = left_side.split('_')[-1].replace('"', '')
+                print('suffix: ', suffix)
+                name = left_side.replace('ID = "STR_UNIT_', '').replace(f'_{suffix}', '').replace('"', '')
+                print('name: ', name)
+                right_side_value = line_split_words_str[1].strip().replace('= ', '').replace('"', '')
+                print('right_side_value: ', right_side_value)
+
+                if name in units_dict:
+                    units_dict[name][suffix] = right_side_value 
+                else:
+                    units_dict[name] = {suffix: right_side_value}
+
+
+            else:
+                left_side = line_split_words[0].strip('').replace('"', '').replace('ID = STR_UNIT_', '') # .strip('ID = "STR_UNIT') #.rstrip('"')
+                print('left_side_split(;): ', left_side)
+                suffix = left_side.split('_')[-1].strip() # [-1]
+                print('suffix: ', suffix)
+                name = left_side.replace(f'_{suffix}', '').strip()
+                print('name: ', name) 
+                
+                print('line_split_words: ', line_split_words)
+                # if len(line_split_words) == 1:
+                #     line_split_words[0]
+                print('line_split_words[1]: ', line_split_words[1], ' ,', type(line_split_words[1]))
+                right_side_value = line_split_words[1].strip().replace('"', '').replace('Str = ', '').replace('Str= ', '')
+                
+                print('suffix: ', suffix, ', right_side_value: ', right_side_value)
+
+                if (name in units_dict): 
+                    units_dict[name][suffix] = right_side_value
+                else:
+                    units_dict[name] = {'NAME': '', 'SR': '', 'LR': ''}
+                    units_dict[name][suffix] = right_side_value
+
+
+for name in units_dict.keys():
+    for key in units_dict[name].keys():
+        print(f'key: {key}, units_dict[{name}][{key}]: {units_dict[name][key]}')
+
+
+ST_units_en_data = json.dumps(units_dict, indent=4)
+
+
+file_path = Path('../extracted_data/ST_units_en_data.json')
+print(file_path.is_file())
+
+# if file_path.is_file():
+with open('../extracted_data/ST_units_en_data.json', 'w') as file:
+    file.write(ST_units_en_data) 
+
+
+
+with open ('../game_files/string_table_en.txt') as str_tbl_file:
+
+    for line in str_tbl_file:
+        # if 'STR_UNIT' in line and ('NAME' in line or 'LR' in line or 'SR' in line):
+        if 'STR_BLD' in line:
+            line_split_words = line.split(';')
+            line_split_words_str = []
+            use_Str_split = False
+            if ';' not in line:
+                line_split_words_str = line.split('Str')
+                use_Str_split = True
+                print('** inside line_split_words_str: ', line_split_words_str)
+            
+            # print('** outside line_split_words_str: ', line_split_words_str)
+            if use_Str_split == True:
+                print('*** inside line split str: ', line_split_words_str)
+                left_side = line_split_words_str[0].strip()
+                suffix = left_side.split('_')[-1].replace('"', '')
+                print('suffix: ', suffix)
+                name = left_side.replace('ID = "STR_BLD_', '').replace(f'_{suffix}', '').replace('"', '')
+                print('name: ', name)
+                right_side_value = line_split_words_str[1].strip().replace('= ', '').replace('"', '')
+                print('right_side_value: ', right_side_value)
+
+                if name in units_dict:
+                    units_dict[name][suffix] = right_side_value 
+                else:
+                    units_dict[name] = {suffix: right_side_value}
+
+
+            else:
+                left_side = line_split_words[0].strip('').replace('"', '').replace('ID = STR_BLD_', '') # .strip('ID = "STR_UNIT') #.rstrip('"')
+                print('left_side_split(;): ', left_side)
+                suffix = left_side.split('_')[-1].strip() # [-1]
+                print('suffix: ', suffix)
+                name = left_side.replace(f'_{suffix}', '').strip()
+                print('name: ', name) 
+                
+                print('line_split_words: ', line_split_words)
+                # if len(line_split_words) == 1:
+                #     line_split_words[0]
+                print('line_split_words[1]: ', line_split_words[1], ' ,', type(line_split_words[1]))
+                right_side_value = line_split_words[1].strip().replace('"', '').replace('Str = ', '').replace('Str= ', '')
+                
+                print('suffix: ', suffix, ', right_side_value: ', right_side_value)
+                print('buildings_dict: ', buildings_dict)
+
+                if (name in buildings_dict): 
+                    buildings_dict[name][suffix] = right_side_value
+                else:
+                    buildings_dict[name] = {'NAME': '', 'SR': '', 'LR': ''}
+                    buildings_dict[name][suffix] = right_side_value
+
+    
+
+ST_buildings_en_data = json.dumps(buildings_dict, indent=4)
+
+
+# file_path = Path('ST_units_en_data.json')
+# print(file_path.is_file())
+
+# if file_path.is_file():
+with open('../extracted_data/ST_buildings_en_data.json', 'w') as file:
+    file.write(ST_buildings_en_data) 
+    
+
+
+with open ('../game_files/string_table_en.txt') as str_tbl_file:
+
+    for line in str_tbl_file:
+        # if 'STR_UNIT' in line and ('NAME' in line or 'LR' in line or 'SR' in line):
+        if 'STR_TECH' in line:
+            line_split_words = line.split(';')
+            line_split_words_str = []
+            use_Str_split = False
+            if ';' not in line:
+                line_split_words_str = line.split('Str')
+                use_Str_split = True
+                print('** inside line_split_words_str: ', line_split_words_str)
+            
+            # print('** outside line_split_words_str: ', line_split_words_str)
+            if use_Str_split == True:
+                print('*** inside line split str: ', line_split_words_str)
+                left_side = line_split_words_str[0].strip()
+                suffix = left_side.split('_')[-1].replace('"', '')
+                print('suffix: ', suffix)
+                name = left_side.replace('ID = "STR_TECH_', '').replace(f'_{suffix}', '').replace('"', '')
+                print('name: ', name)
+                right_side_value = line_split_words_str[1].strip().replace('= ', '').replace('"', '')
+                print('right_side_value: ', right_side_value)
+
+                if name in units_dict:
+                    units_dict[name][suffix] = right_side_value 
+                else:
+                    units_dict[name] = {suffix: right_side_value}
+
+
+            else:
+                left_side = line_split_words[0].strip('').replace('"', '').replace('ID = STR_TECH_', '') # .strip('ID = "STR_UNIT') #.rstrip('"')
+                print('left_side_split(;): ', left_side)
+                suffix = left_side.split('_')[-1].strip() # [-1]
+                print('suffix: ', suffix)
+                name = left_side.replace(f'_{suffix}', '').strip()
+                print('name: ', name) 
+                
+                print('line_split_words: ', line_split_words)
+                # if len(line_split_words) == 1:
+                #     line_split_words[0]
+                print('line_split_words[1]: ', line_split_words[1], ' ,', type(line_split_words[1]))
+                right_side_value = line_split_words[1].strip().replace('"', '').replace('Str = ', '').replace('Str= ', '')
+                
+                print('suffix: ', suffix, ', right_side_value: ', right_side_value)
+                print('techs_dict: ', techs_dict)
+
+                if (name in techs_dict): 
+                    techs_dict[name][suffix] = right_side_value
+                else:
+                    techs_dict[name] = {'NAME': '', 'SR': '', 'LR': ''}
+                    techs_dict[name][suffix] = right_side_value
+
+    
+
+ST_techs_en_data = json.dumps(techs_dict, indent=4)
+
+
+# file_path = Path('ST_units_en_data.json')
+# print(file_path.is_file())
+
+# if file_path.is_file():
+with open('../extracted_data/ST_techs_en_data.json', 'w') as file:
+    file.write(ST_techs_en_data) 
+
+
+
