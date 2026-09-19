4c01b539b5b206719874892a69fff418318691aa
sebG <seb.goodchild@gmail.com>
2026-09-19 15:47:31 -0400

    Layout changes to legend display content. Most notably it always displays the items in three columns. Might want to add media query for display content font size.

--- docs/js/colorSchemeToggle.js
+++ docs/js/colorSchemeToggle.js
@@ -7,6 +7,7 @@ const majorGodDescriptionEl = document.getElementById('side_panel__major_god_des
 const sidePanelMinorGods = document.getElementById('side_panel__minor_gods');
 const helpTextEl = document.getElementById('helptext');
 const helpTextSPEl = document.getElementById('helptext_SP');
+const legendDisplayEl = document.getElementById('legend-items__container');
 const ageSeperationLineMinorGodTree = document.getElementsByClassName('ageSeperationLine_minorGodTree');
 const rootEl = document.documentElement;
 const LIGHT_SCHEME_TEXT_COLOR = 'rgb(16, 16, 16)';
@@ -63,6 +64,8 @@ function updateColorScheme() {
         helpTextEl.style.background = '#727477';
         helpTextSPEl.style.color = LIGHT_SCHEME_TEXT_COLOR;
         helpTextSPEl.style.background = '#727477';
+        legendDisplayEl.style.color = LIGHT_SCHEME_TEXT_COLOR;
+        legendDisplayEl.style.background = '#727477';
     }
     else {
         localStorage.setItem('colorScheme', 'dark');
@@ -89,6 +92,8 @@ function updateColorScheme() {
         helpTextEl.style.background = '#241f38';
         helpTextSPEl.style.color = '#d1cfd9';
         helpTextSPEl.style.background = '#241f38';
+        legendDisplayEl.style.color = '#d1cfd9';
+        legendDisplayEl.style.background = '#241f38';
     }
 }
 export {};
--- docs/js/legend.js
+++ docs/js/legend.js
@@ -1,8 +1,4 @@
 import { CARET_TYPES } from "./techtree.js";
-const legendItemsDiv = document.getElementById('legend-items__container');
-const legendContainerdiv = document.getElementById('legend-button-container__fixed');
-const legendItemsDivLeft = document.createElement('div');
-const legendItemsDivRight = document.createElement('div');
 const COST_ICONS = [
     'wood',
     'food',
@@ -38,15 +34,25 @@ const STATS_ICONS = [
     'range',
     'recharge_time',
 ];
+const legendItemsDiv = document.getElementById('legend-items__container');
+const legendContainerdiv = document.getElementById('legend-button-container__fixed');
+const legendItemsDivLeft = document.createElement('div');
+const legendItemsDivCenter = document.createElement('div');
+const legendItemsDivRight = document.createElement('div');
+// 587px
 // cost_str += `<span class="cost food " title="${unit_data.Food_Cost} Food">${unit_data.Food_Cost}</span> `;
+// add i.e. movement speed to velocity 
 const legendItemsTotalCount = Object.keys(CARET_TYPES).length + COST_ICONS.length + STATS_ICONS.length - 1;
 let legendItemsAddedCount = 0;
+const legendItemsCountOneThird = legendItemsTotalCount / 3;
+console.log('UIU legendItemsTotalCount: ', legendItemsTotalCount); // 37
+console.log('UIU legendItemsCountOneThird: ', legendItemsCountOneThird); // 12.333
 for (const caret_type in CARET_TYPES) {
     if (caret_type === 'BLANK') {
         continue;
     }
-    console.log('UIU', caret_type);
-    console.log('UIU', CARET_TYPES[caret_type]);
+    // console.log('UIU', caret_type);
+    // console.log('UIU', CARET_TYPES[caret_type]);
     // lengendItemsDiv.innerHTML += `<br> ${CARET_TYPES[caret_type].name}`;
     const newCaretTypeContainerDiv = document.createElement('div');
     // const newCaretColourDiv = document.createElement('div');
@@ -62,8 +68,11 @@ for (const caret_type in CARET_TYPES) {
     const caretTypeTextdiv = document.createElement('div');
     caretTypeTextdiv.innerHTML = `${CARET_TYPES[caret_type].name.replaceAll('_', ' ')}`;
     newCaretTypeContainerDiv.append(caretTypeTextdiv);
-    if (legendItemsAddedCount < legendItemsTotalCount / 2) {
+    if (legendItemsAddedCount < legendItemsCountOneThird) {
         legendItemsDivLeft.appendChild(newCaretTypeContainerDiv);
+    }
+    else if (legendItemsAddedCount < legendItemsCountOneThird * 2) {
+        legendItemsDivCenter.appendChild(newCaretTypeContainerDiv);
     }
     else {
         legendItemsDivRight.appendChild(newCaretTypeContainerDiv);
@@ -74,33 +83,42 @@ for (const caret_type in CARET_TYPES) {
     console.log('UIU divheight', curDivHeight);
 }
 for (const cost_icon_name of COST_ICONS) {
-    const newLegendIcon = document.createElement('div');
-    newLegendIcon.innerHTML += `<span class="cost ${cost_icon_name}" title="${cost_icon_name} Cost">${cost_icon_name.replaceAll('_', ' ')}</span>`;
-    if (legendItemsAddedCount < legendItemsTotalCount / 2) {
-        legendItemsDivLeft.appendChild(newLegendIcon);
+    const newLegendIconDiv = document.createElement('div');
+    newLegendIconDiv.innerHTML += `<span class="cost ${cost_icon_name}" title="${cost_icon_name} Cost">${cost_icon_name.replaceAll('_', ' ')}</span>`;
+    if (legendItemsAddedCount < legendItemsCountOneThird) {
+        legendItemsDivLeft.appendChild(newLegendIconDiv);
+    }
+    else if (legendItemsAddedCount < legendItemsCountOneThird * 2) {
+        legendItemsDivCenter.appendChild(newLegendIconDiv);
     }
     else {
-        legendItemsDivRight.appendChild(newLegendIcon);
+        legendItemsDivRight.appendChild(newLegendIconDiv);
     }
     legendItemsAddedCount++;
 }
 // stat_str += `<span class="stat hitpoints" title="${unit_data.Hitpoints} Hitpoints"> ${unit_data.Hitpoints}, </span>`;
 for (const stat_icon_name of STATS_ICONS) {
-    const newLegendIcon = document.createElement('div');
-    newLegendIcon.innerHTML += `<span class="stat ${stat_icon_name}" title="${stat_icon_name}">   ${stat_icon_name.replaceAll('_', ' ')}</span>`;
-    if (legendItemsAddedCount < legendItemsTotalCount / 2) {
-        legendItemsDivLeft.appendChild(newLegendIcon);
+    const newLegendIconDiv = document.createElement('div');
+    newLegendIconDiv.innerHTML += `<span class="stat ${stat_icon_name}" title="${stat_icon_name}">   ${stat_icon_name.replaceAll('_', ' ')}</span>`;
+    if (legendItemsAddedCount < legendItemsCountOneThird) {
+        legendItemsDivLeft.appendChild(newLegendIconDiv);
+    }
+    else if (legendItemsAddedCount < legendItemsCountOneThird * 2) {
+        legendItemsDivCenter.appendChild(newLegendIconDiv);
     }
     else {
-        legendItemsDivRight.appendChild(newLegendIcon);
+        legendItemsDivRight.appendChild(newLegendIconDiv);
     }
     legendItemsAddedCount++;
 }
 legendItemsDiv.appendChild(legendItemsDivLeft);
+legendItemsDiv.appendChild(legendItemsDivCenter);
 legendItemsDiv.appendChild(legendItemsDivRight);
 const legendButton = document.getElementById('legend-button');
 legendButton.onclick = (e) => {
     e.preventDefault();
+    const screenHeight = document.querySelector('body').offsetHeight;
+    console.log('UIU screenHeight: ', screenHeight);
     if (legendButton.getAttribute('data-state') == 'hidden') {
         legendButton.setAttribute('data-state', 'displayed');
         // legendItemsDiv.style.display = 'block';
